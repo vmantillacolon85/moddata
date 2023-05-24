084f1ea707553b6f6b7c286bc0de55c8671dedf7
Victor Mantilla Colon <72588367+vmantillacolon85@users.noreply.github.com>
2023-05-23 22:44:50 -0400

    Add files via upload

--- SQLWindowFunctionsPractice.sql
+++ SQLWindowFunctionsPractice.sql
@@ -1,148 +1,111 @@
---1. Using Invoice Table show average total per country
+--1. List product_name, product category,  unit_price, 
+--and average unit price for that category
 
-SELECT BillingCountry, ROUND(AVG(Total), 2) AS TotalPerCountry
-FROM Invoice i 
-GROUP BY BillingCountry
-ORDER BY 2 DESC
-
--- 2. Create a table with the following columns InvoiceId, BillingCountry, Total, Avg_Total_for_Planet
--- for each InvoiceID
-
-SELECT InvoiceID, BillingCountry, Total, 
-AVG(Total) OVER() AS AvgTotalForPlanet
-FROM Invoice i
-
-SELECT InvoiceID, BillingCountry, Total, 
-SUM(Total) OVER() AS SumTotalForPlanet
-FROM Invoice i --this allows us to see the whole table and the rows
-
-SELECT SUM(Total)
-FROM Invoice i --this only shows one number on one row
+SELECT p.product_name, c.category_name, p.unit_price,
+round(CAST(avg(unit_price) OVER (PARTITION BY c.category_id) AS NUMERIC),2) AS avg_unit_price
+FROM products p
+JOIN categories c ON p.category_id = c.category_id
 
 
-WITH Country_Totals AS
-(SELECT InvoiceID, BillingCountry, Total, 
-SUM(Total) OVER() AS SumTotalForPlanet
-FROM Invoice i)
+--2. For each product, list the product name, the units on order, 
+--the supplier company name, and the total units on order for that supplier
 
-SELECT *, ROUND((Total/SumTotalForPlanet) * 100, 2) PercentOfTotal
-FROM Country_Totals
-ORDER BY 5 DESC
-
-
----
--- 3. Create a table with the following columns InvoiceId, BillingCountry, Total, Avg_Total_for_Planet
--- for each InvoiceID
---STEPS
-
---Traditional Aggregate
-SELECT InvoiceID, BillingCountry, Total,
-SUM(Total)
-FROM Invoice i
-GROUP BY BillingCountry --this results in the code running one line per country not all the totals in the country
-
-
---Empty Bracket Window Function
-SELECT InvoiceId, BillingCountry, Total,
-SUM(Total) OVER() as TotalForWorld
-FROM Invoice i 
-
-
---Window Function with Partition 
-SELECT InvoiceId, BillingCountry, Total,
-SUM(Total) OVER(PARTITION BY BillingCountry) as TotalPerCountry
-FROM Invoice i  --this results in all the totals of the country
---ORDER BY 4 DESC
-
-
-SELECT t.Composer, SUM(t.UnitPrice) 
-FROM InvoiceLine il 
-JOIN Track t 
-ON il.TrackId = t.TrackId
-GROUP BY t.Composer
-ORDER BY 2 DESC
-
-
-SELECT il.InvoiceId, t.Composer, t.UnitPrice,
-SUM(t.UnitPrice) OVER()
-FROM InvoiceLine il 
-JOIN Track t 
-ON il.TrackId = t.TrackId
-
-SELECT il.InvoiceId, t.Composer, t.UnitPrice,
-SUM(t.UnitPrice) OVER(PARTITION BY t.Composer)
-FROM InvoiceLine il 
-JOIN Track t 
-ON il.TrackId = t.TrackId
-WHERE t.Composer IS NOT NULL
-
-SELECT il.InvoiceId, t.Composer, t.UnitPrice,
-SUM(t.UnitPrice) OVER(PARTITION BY t.Composer) AS TotalPerArtist,
-SUM(t.UnitPrice) OVER() as Total
-FROM InvoiceLine il 
-JOIN Track t 
-ON il.TrackId = t.TrackId
-WHERE t.Composer IS NOT NULL
-
-
-------------------------------------------------------------------
+select 
+from products p 
 
 
 
 
----4. Create a table with the following columns InvoiceID, BillingCountry, 
---Total, Max_Total_per country,
---and the difference between total and max total
 
-WITH cte AS 
-( SELECT InvoiceId, BillingCountry, Total,
-MAX(Total) OVER(PARTITION BY BillingCountry) AS MaxPerCountry
-From Invoice i 
-)
+--3. For each customer, list the customer company name, 
+--the counter of orders for that customer, and the total
+--number of orders for that supplier 
 
-SELECT *, MaxPerCountry -Total
-FROM cte 
-
-
---5.Create a table showing the BillingCountry and the Average difference from Average per country
-
-WITH cte AS 
-( SELECT InvoiceId, BillingCountry, Total,
-Avg(Total) OVER(PARTITION BY BillingCountry) AS AvgPerCountry
-From Invoice i 
-)
-,
-cte2 AS
+with customer_orders as 
 (
-SELECT *, Total - AvgPerCountry AS Difference
-FROM CTE
+select c.company_name, c.country, count(o.order_id) as num_orders
+from customers c
+left join orders o 
+on c.customer_id = o.customer_id 
+group by c.company_name, c.country 
 )
-SELECT *,
-AVG(Difference) OVER (PARTITION BY BillingCountry) AS AvgDifferencePerCountry
-FROM cte2
+select company_name, country, num_orders,
 
 
---another solution: 
 
-WITH cte AS
+--4.  For each order, list the customer, calculate the total $ for the order, 
+--and the total $ for spent by that customer.
+--the total $ for each order will use the order_details table and 
+--be the sum of (unit_price * quantity * (1-discount))
+
+with order_total as
 (
-SELECT BillingCountry,Total,
-AVG(Total) OVER (PARTITION BY BillingCountry) AS AvgPerCountry
-FROM Invoice i
+select o.order_id, c.company_name, 
+sum(od.unit_price * od.quantity*(1-od.discount)) as order_amount
+from orders o 
+join customers c
+on o.customer_id = c.customer_id 
+join order_details od 
+on o.order_id = od.order_id 
+group by o.order_id, c.company_name 
+order by o.order_id 
 )
-SELECT BillingCountry, AvgPerCountry, (AvgPerCountry - Total) AS Difference,
-AVG(AvgPerCountry - Total) OVER (PARTITION BY BillingCountry)
-FROM cte
+select order_id, company_name, order_amount,
+round(cast(sum(order_amount) over (partition by company_name) as numeric), 2)
+from order_total 
 
---a solution to a similar but different question
 
-WITH cte AS 
+--5. For each product list the name, the category, the total amount of $ spent by customers for that product, 
+--and the total amount of $ spent by customers for its category. 
+
+
+with total_sales as 
 (
-SELECT InvoiceId, BillingCountry, Total,
-AVG(Total) OVER(PARTITION BY BillingCountry) AS AvgPerCountry,
-AVG(Total) OVER() AS AvgForAllCountries -- this IS for ALL countries...
-FROM Invoice i
+select p.product_name, c.category_name, 
+sum(od.unit_price*od.quantity*(1-od.discount)) as product_sales
+from products p
+join categories c 
+on p.category_id = c.category_id 
+left join order_details od 
+on od.product_id = p.product_id 
+group by p.product_name, c.category_name
 )
-SELECT *, AvgForAllCountries - AvgPerCountry AS AvgDifference
-FROM cte
+select product_name, category_name, product_sales,
+sum(product_sales) over (partition by category_name) as category_total
+from total_sales
+
+--6.Use ROW_NUMBER to assign a value (1, 2, 3, 4) to each of our shipping companies, 
+--from most orders to least orders.  
+--*note that the ship_via field in ‘orders’ maps to the shipper_id field in ‘shippers’
+
+select s.company_name, 
+count(o.order_id),
+row_number() over (order by count(o.order_id) desc)
+from shippers s 
+left join orders o 
+on s.shipper_id = o.ship_via
+group by s.company_name 
+
+--7. Use DENSE_RANK() to designate our top customers from each country, 
+--from most money spent to least amount of money spent.  
+
+
+select c.country, c.company_name,
+sum(od.unit_price * od.quantity * (1-od.discount)) as customer_total,
+dense_rank () over (partition by c.country order by sum(od.unit_price * od.quantity * (1-od.discount)))
+from customers c
+join orders o 
+on c.customer_id = o.customer_id 
+join order_details od 
+on o.order_id = od.order_id 
+group by c.country, c.company_name 
+order by c.country
+
+
+
+
+
+
+
+
 
